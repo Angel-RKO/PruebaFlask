acb31b823e7a2f1ed0d29a46aa92fcb499d5cfde
Angel Rico <wicholenovo.1@gmail.com>
2022-08-30 17:32:40 -0500

    Video implementado

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
+import requests
+
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    
+    datosObtenidos = requests.get('https://api.dailymotion.com/videos?channel=music&limit=10&search=Folk')
+    datosOnJSON = datosObtenidos.json()
+    print(datosOnJSON)
+
+    return render_template('index.html', datos=datosOnJSON['list'])
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
